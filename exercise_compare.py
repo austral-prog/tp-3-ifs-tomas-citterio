bb1f1f936f9fae3a5f74928374648cee2633aba4
tomas-citterio <tomcitterio08@gmail.com>
2026-04-06 17:55:17 -0300

    Update exercise_compare.py

--- exercise_compare.py
+++ exercise_compare.py
@@ -15,4 +15,12 @@ def compare():
         Para las entradas "7" y "7", la salida esperada es:
         7 es igual a 7
     """
-    pass
+    a = int(input())
+    b = int(input())
+
+    if a > b:
+        print(f"{a} es mayor que {b}")
+    elif a < b:
+        print(f"{a} es menor que {b}")
+    else:
+        print(f"{a} es igual a {b}")
