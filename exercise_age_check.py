cdc308d294cbd93c7e123ef065e0d77fc2e97572
tomas-citterio <tomcitterio08@gmail.com>
2026-04-06 17:50:18 -0300

    Update exercise_age_check.py

--- exercise_age_check.py
+++ exercise_age_check.py
@@ -18,4 +18,12 @@ def age_check():
         Para las entradas "-5" y "18", la salida esperada es:
         Entrada invalida
     """
-    pass
+    edad = int(input())
+    limite = int(input())
+
+    if edad <= 0 or limite <= 0:
+        print("Entrada invalida")
+    elif edad >= limite:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
